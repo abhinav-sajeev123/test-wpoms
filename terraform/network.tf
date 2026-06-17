c028dc6f59beb96643f3edfd07c593ec3b8ad08c
abhinav-sajeev123 <abhinavsajeevabhi@gmail.com>
2026-06-18 01:33:57 +0530

    port added

--- terraform/network.tf
+++ terraform/network.tf
@@ -28,6 +28,20 @@ resource "aws_security_group" "ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5173
+    to_port     = 5173
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
