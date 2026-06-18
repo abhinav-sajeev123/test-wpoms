0ba7c3f2ff8ea25311828d5915a9b3d0a8c5bdf9
abhinav-sajeev123 <abhinavsajeevabhi@gmail.com>
2026-06-18 15:17:17 +0530

    fix done

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,6 @@ echo "Running deployment script..."
 echo "Downloading docker-compose..."
 aws s3 cp s3://$S3_BUCKET/docker-compose.yml docker-compose.yml
 
-echo "Fetching DB config from SSM..."
-POSTGRES_DB=$(aws ssm get-parameter --name "/wpoms/POSTGRES_DB" --query "Parameter.Value" --output text)
-
-POSTGRES_USER=$(aws ssm get-parameter --name "/wpoms/POSTGRES_USER" --query "Parameter.Value" --output text)
-
-POSTGRES_PASSWORD=$(aws ssm get-parameter --name "/wpoms/POSTGRES_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
-
 echo "Logging into ECR..."
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
 
